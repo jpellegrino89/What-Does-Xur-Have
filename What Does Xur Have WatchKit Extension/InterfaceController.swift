0257f6094e4f9f23c3f71afe87054de22f5d8266
Joseph Pellegrino <jpellegrino89@gmail.com>
2016-01-16 10:01:03 -0500

    Initial Commit

---
+++ What Does Xur Have WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  What Does Xur Have WatchKit Extension
+//
+//  Created by Joseph Pellegrino on 1/16/16.
+//  Copyright © 2016 Joseph Pellegrino. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
